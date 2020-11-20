816622344609c15f97ffa3860739e7a70d81ff28
Andres Carrillo <andresjoe24@gmail.com>
2020-11-19 21:23:18 -0500

    [ADD] Enpoint to validate user by qr scan

---
+++ src/controller/qr.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response, Router } from 'express';
+
+
+class QRController {
+
+    router: Router;
+
+    constructor(){
+        this.router = Router();
+        this.routes();
+    }
+
+    async getDataScanQR(req: Request, res: Response){
+
+    }
+
+    async validateUserQR(req: Request, res: Response){
+
+    }
+ 
+    routes() {
+        this.router.get("/", this.getDataScanQR);
+        this.router.post("/", this.validateUserQR);   
+    }
+}
+
+const qrController = new QRController();
+export default qrController.router;
